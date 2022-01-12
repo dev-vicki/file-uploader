e18950e911f2658fd8f97e7c8de67f399b46f51b
dev-vicki <vignesh.r1999@gmail.com>
2022-01-12 12:42:39 +0530

    implemented supported file extensions and  preview

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const dragArea = document.querySelector(".drag-area");
 const dragText = document.querySelector(".header");
+let file;
 
 // inside drag area
 dragArea.addEventListener("dragover", (event) => {
     event.preventDefault();
     dragText.textContent = "Release to upload";
     dragArea.classList.add("active");
-    // console.log("file is inside drag area!");
+
 })
 
 // outside drag area
@@ -14,12 +15,37 @@ dragArea.addEventListener("dragleave", (event) =>{
     dragText.textContent = "Drag & Drop";
     dragArea.classList.remove("active");
 
-    // console.log("file left drag area!")
+
 })
 
 // dropped inside drag area
 dragArea.addEventListener("drop", (event) =>{
-    event.preventDefault();
-    // console.log("file dropped inside drag area!")
+  event.preventDefault();
+
+  // only single file is accepted at a time
+  file = event.dataTransfer.files[0];
+  // console.log(file)
+
+  let fileType = file.type;
+  // console.log(fileType)
+
+  let validExtensions = ["image/jpeg", "image/jpg", "image/png", "text/html","text/css", "text/plain", "application/pdf"];
+
+  if(validExtensions.includes(fileType)) {
+      let fileReader = new FileReader();
+
+      fileReader.onload = () =>{
+          let fileURL = fileReader.result;
+          //console.log(fileURL);
+
+          let imgTag = `<img src="${fileURL}" alt="">`;
+          dragArea.innerHTML = imgTag;
+      };
+      fileReader.readAsDataURL(file)
+  } else {
+      alert('Entered file format not supported!');
+      dragArea.classList.remove('active');
+  }
+
 })
 
