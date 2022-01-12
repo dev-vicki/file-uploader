a8dc2710950f52742fe9be43cf964361ea3b7057
dev-vicki <vignesh.r1999@gmail.com>
2022-01-12 12:10:09 +0530

    border changes when file dragged inside dragArea.

--- app.js
+++ app.js
@@ -5,12 +5,15 @@ const dragText = document.querySelector(".header");
 dragArea.addEventListener("dragover", (event) => {
     event.preventDefault();
     dragText.textContent = "Release to upload";
-     // console.log("file is inside drag area!");
+    dragArea.classList.add("active");
+    // console.log("file is inside drag area!");
 })
 
 // outside drag area
 dragArea.addEventListener("dragleave", (event) =>{
     dragText.textContent = "Drag & Drop";
+    dragArea.classList.remove("active");
+
     // console.log("file left drag area!")
 })
 
